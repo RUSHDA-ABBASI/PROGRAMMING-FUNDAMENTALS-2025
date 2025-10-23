5dbc634a01ddf0464609b0ff677669950c98e97d
RUSHDA-ABBASI <rush.cs19@gmail.com>
2025-10-23 01:26:14 -0700

    Add files via upload

---
+++ factorialOfaNumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("factorial is not for negative numbers.\n");
+        return 0;
+    }
+
+    for (i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d = %d\n", n, factorial);
+    return 0;
+}
